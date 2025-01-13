fc8b30e711564c28dbef6eaa8717584672c55130
John Tranier <john.tranier@isae-supaero.fr>
2025-01-13 10:35:12 +0100

    Create empty Spring Boot web app

---
+++ src/main/kotlin/com/elaastic/keypoc/KeypocApplication.kt
@@ -0,0 +1,11 @@
+package com.elaastic.keypoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeypocApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeypocApplication>(*args)
+}
---
+++ src/test/kotlin/com/elaastic/keypoc/KeypocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.elaastic.keypoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeypocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
